081babf49d4629c46b66c8c13203b16887189fdd
stephenmunyi <stephenmunyi075@gmail.com>
2024-05-07 03:04:09 +0300

    commit

--- Bot Battlr/src/components/Battlritem.jsx
+++ Bot Battlr/src/components/Battlritem.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import './Battlritem.css'
 function Battlritem({ battlr }) {
   return (
-    <div>
-      <img src={battlr.avatar_url} alt="" />
-      <h1>{battlr.name}</h1>
+    <div className="item">
+      <img className="itemImage" src={battlr.avatar_url} alt="" />
+     
+      <h1> {battlr.name} </h1>
+      <h4> {battlr.catchphrase} </h4>
+      <h3> health {battlr.health} </h3>
+     <h5> armor {battlr.armor} </h5>
+     <h5> damage {battlr.damage} </h5>
+
+
+
       <button>view Robot</button>
     </div>
   );
--- Bot Battlr/src/components/Search.jsx
+++ Bot Battlr/src/components/Search.jsx
@@ -1,29 +1,29 @@
-import { useEffect, useState } from "react"
-const URL = "http://localhost:3000/bots"
+import { useEffect, useState } from "react";
+import "./Search.css";
+const URL = "http://localhost:3000/bots";
 
-function Search({battlrData,setBattlrData}) {
+function Search({ battlrData, setBattlrData }) {
   const [query, setQuery] = useState("");
-  useEffect(()=> {
-   
-        async function fetchBattlr
-        (){
-            
-            const res= await fetch(`${URL} `)
-            const data= await res.json () 
-            console.log(data)
-            setBattlrData(data)
-          }
-           fetchBattlr()
-      },[query],)
- 
-    
+  useEffect(() => {
+    async function fetchBattlr() {
+      const res = await fetch(`${URL} `);
+      const data = await res.json();
+      console.log(data);
+      setBattlrData(data);
+    }
+    fetchBattlr();
+  }, [query]);
 
   return (
-    <div>
-      <input value={query} onChange={(e) => setQuery(e.target.value)} 
-      type="text"
+    <div className="searchContainer">
+      <input
+      placeholder="search"
+      className="enter"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        type="text"
       />
     </div>
-  )
+  );
 }
-export default Search
+export default Search;
